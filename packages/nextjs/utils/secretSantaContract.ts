dc09e82c7539118d40e0299bcf0c0fec9aee1023
Alexandre Carvalheira <axxcar@gmail.com>
2025-12-17 22:57:55 -0300

    new contract deployed

--- packages/nextjs/utils/secretSantaContract.ts
+++ packages/nextjs/utils/secretSantaContract.ts
@@ -1,7 +1,7 @@
 // Secret Santa Contract - deployed on Arbitrum Sepolia
 // TODO: Update this address after deploying the updated contract with name support
 export const CONTRACT_ADDRESS =
-  "0x78a8fe48995bC963596F7175B6bECa66fE075142" as const;
+  "0x64bd6AB8Bcee6E0D1C4B1AAFAd5B4fcF9A8ED4Cf" as const;
 
 // Game states
 export enum GameState {
